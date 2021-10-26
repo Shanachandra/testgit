8cc28aa3f2e62d8538eb3949f962e276a98959d3
ShanaChandra <rishikabtech1998@gmail.com>
2021-10-26 12:41:41 +0530

    add movie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Button from '@mui/material/Button';
 
 const movies = [
   {
@@ -49,20 +50,9 @@ function App() {
   return (
     
     <div className="App"> 
-    <AddMovie />
+    {/* <AddMovie /> */}
     
-     <div className="movie-list">
-       
-        {movies.map((mv,index) => (
-          <Movie
-            key={index}          
-            name={mv.movieName}
-            poster={mv.moviePic}
-            rating={mv.imdb}
-            summary={mv.movieSummary}
-          />
-        ))}
-      </div>  
+    <MovieList />
       {/* {movies.map((movie) => (
         <Msg
           name={movie.movieName}
@@ -71,8 +61,8 @@ function App() {
           imdb={movie.imdb}
         />
       ))} */}
-{/* <ColorList />
-      <ColorBox /> */}
+<ColorList />
+      <ColorBox />
     </div>
   );
 }
@@ -135,6 +125,7 @@ function ColorList()
         value={color}
         style={styles}
       />
+      <Button variant="text">Text</Button>
       <button onClick={()=>setcolors([...colors,color])}>Add Color</button>
       {colors.map((clr,index)=>(
       <ColorBox key={index} color={clr} />
@@ -162,11 +153,95 @@ function ColorBox({color})
 //     </div>
 //   );
 // }
+function MovieList()
+{
+  const intial_movies = [
+    {
+      movieName: "The Pursuit of Happiness",
+      moviePic:
+        "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/thepursuitofhappyness_onesheet_1400x2100.png?itok=BSpvsOsJ",
+      movieSummary:
+        "Tired of Chris's professional failures, his wife decides to separate, leaving him financially broke with an unpaid internship in a brokerage firm and his son's custody to deal with.",
+      imdb: "8"
+    },
+  
+    {
+      movieName: "The Imitation Game",
+      moviePic:
+        "https://m.media-amazon.com/images/M/MV5BOTgwMzFiMWYtZDhlNS00ODNkLWJiODAtZDVhNzgyNzJhYjQ4L2ltYWdlXkEyXkFqcGdeQXVyNzEzOTYxNTQ@._V1_.jpg",
+      movieSummary:
+        "Alan Turing, a British mathematician, joins the cryptography team to decipher the German enigma code. With the help of his fellow mathematicians, he builds a machine to crack the codes.",
+      imdb: "8"
+    },
+    {
+      movieName: "Matrix",
+      moviePic:
+        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg",
+      movieSummary:
+        "Thomas Anderson, a computer programmer, is led to fight an underground war against powerful computers who have constructed his entire reality with a system called the Matrix.",
+      imdb: "8.7"
+    },
+    {
+      movieName: "Intersterllar",
+      moviePic: "https://pbs.twimg.com/media/EmUeXUZVcAA5v6j.jpg",
+      movieSummary:
+        "When  Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
+      imdb: "8.6"
+    },
+    {
+      movieName: "Ice Age",
+      moviePic:
+        "https://lumiere-a.akamaihd.net/v1/images/p_iceage_21384_4689eb31.jpeg?region=0%2C0%2C540%2C810",
+      movieSummary:
+        "Manny the mammoth, Sid the loquacious sloth, and Diego the sabre-toothed tiger go on a comical quest to return a human baby back to his father, across a world on the brink of an ice age.",
+      imdb: "7.5"
+    }
+  ];
+  const [movies,setMovies]=useState(intial_movies)
+  const [movieName,setName]=useState('');
+  const [moviePic,setPoster]=useState('');
+  const [imdb,setRating]=useState('');
+  const [movieSummary,setSummary]=useState('');
+  return(
+    <section>
+      <div className="add-movie-form">
+        <input placeholder="Name" value={movieName} onChange={(event)=>setName(event.target.value)}/>
+        <input placeholder="Poster Url" value={moviePic}  onChange={(event)=>setPoster(event.target.value)}/>
+        <input placeholder="Rating" value={imdb}  onChange={(event)=>setRating(event.target.value)}/>
+        <input placeholder="Summary" value={movieSummary} onChange={(event)=>setSummary(event.target.value)}/>
+        <button onClick={()=>
+          {
+            const newmovie={movieName,moviePic,imdb,movieSummary}
+            setMovies([...movies,newmovie])
+            setName('');
+            setPoster('');
+            setRating('');
+            setSummary('');
+
+          }}>Add Movie</button>
+      </div>
+    
+  <div className="movie-list">
+       
+  {movies.map((mv,index) => (
+    <Movie
+      key={index}          
+      name={mv.movieName}
+      poster={mv.moviePic}
+      rating={mv.imdb}
+      summary={mv.movieSummary}
+    />
+  ))}
+</div> 
+</section>
+  ) 
+}
 function Movie({name, poster, rating, summary }) {  
-  const [show,setShow]=useState(false )
+  const [show,setShow]=useState(true )
   
   const styles={display:show? "block":"none"}
   return (
+    
     <div className="movie-container">
       <img className="movie-poster" src={poster} alt={name} />
       <div className="movie-specs">
